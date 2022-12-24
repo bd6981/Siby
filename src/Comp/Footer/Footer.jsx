58822f9f17b19616d3d39dea2b5d8b9146e1e19d
bd6981 <brittanyjdavis22@gmail.com>
2022-12-23 22:48:16 -0500

    completed footer jsx

--- src/Comp/Footer/Footer.jsx
+++ src/Comp/Footer/Footer.jsx
@@ -1,4 +1,22 @@
 import React from 'react'
-import "./Footer.css"
+import './Footer.css'
 import {BsGithub} from "react-icon/bs"
 import {AiOutlineLinkedin} from 'react-icons/ai'
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='footer-container'>
+                <div className='item2'>
+                    <p>Brittany Davis</p>
+                    <a href='http://github.com/bd6981'
+                    target='_blank'
+                    className='item1'><BsGithub/></a>
+                    <a href='http://www.linkedin.com/in/brittany~davis/'
+                    target='_blank'
+                    className='item1'><AiOutlineLinkedin/></a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
