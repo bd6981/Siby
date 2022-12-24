73555ff50c0e9d3e028bae21c823e61b291a213a
bd6981 <brittanyjdavis22@gmail.com>
2022-12-23 23:34:27 -0500

    added to header.jsx

--- src/Comp/Header/Header.jsx
+++ src/Comp/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='Header'>
+      <header>
+        <h1>Header</h1>
+      </header>
     </div>
   )
 }
-
 export default Header
