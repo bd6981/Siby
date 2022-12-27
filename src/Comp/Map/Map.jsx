475a63e724d90a8eb49fffcb727314a0ff73eaf6
bd6981 <brittanyjdavis22@gmail.com>
2022-12-26 21:49:57 -0500

    working on onLoad in map.jsx

--- src/Comp/Map/Map.jsx
+++ src/Comp/Map/Map.jsx
@@ -17,7 +17,19 @@ function MyComponent() {
     googleMapsApiKey: "AIzaSyAPntdW_JA88-CRNwjhbR2d6Pr3e8f74II"
   })
 
+  const [map, setMap] = React.useState(null)
 
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
 
   return isLoaded ? (
       <GoogleMap
