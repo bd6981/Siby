deaed0286c0f9cb4d4754c84ba0231e0765398e4
bd6981 <brittanyjdavis22@gmail.com>
2022-12-26 21:49:02 -0500

    adding map container

--- src/Comp/Map/Map.jsx
+++ src/Comp/Map/Map.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
 
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
