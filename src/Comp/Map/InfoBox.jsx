d9b951c71c4998b6b778f791fff344164e4e5f06
bd6981 <brittanyjdavis22@gmail.com>
2022-12-31 21:22:19 -0500

    added ibr to info

--- src/Comp/Map/InfoBox.jsx
+++ src/Comp/Map/InfoBox.jsx
@@ -16,7 +16,7 @@ const InfoBox = () => {
                 <li>ID: <strong>{info.beat}</strong></li>
                 <li>ID: <strong>{info.zone}</strong></li>
                 <li>ID: <strong>{info.location}</strong></li>
-                <li>ID: <strong>{info.zone}</strong></li>
+                <li>ID: <strong>{info.ibr_code}</strong></li>
                 <li>ID: <strong>{info.zone}</strong></li>
                 <li>ID: <strong>{info.zone}</strong></li>
                 <li>ID: <strong>{info.zone}</strong></li>
