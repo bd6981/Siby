907d5cb0c984df4b568e1fc520979c75af636ad2
bd6981 <brittanyjdavis22@gmail.com>
2022-12-31 21:34:00 -0500

    added NPU TO INFO BOX

--- src/Comp/Map/InfoBox.jsx
+++ src/Comp/Map/InfoBox.jsx
@@ -19,7 +19,7 @@ const InfoBox = () => {
                 <li>IBR CODE: <strong>{info.ibr_code}</strong></li>
                 <li>CRIME: <strong>{info.name}</strong></li>
                 <li>NEIGHBORHOOD: <strong>{info.neighborhood}</strong></li>
-                <li>ID: <strong>{info.npu}</strong></li>
+                <li>NPU: <strong>{info.npu}</strong></li>
                 <li>ID: <strong>{info.lat}</strong></li>
                 <li>ID: <strong>{info.lng}</strong></li>
             </ul>
