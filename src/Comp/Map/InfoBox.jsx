39845aa30c085bf8ee55918b3810a48c8c168b78
bd6981 <brittanyjdavis22@gmail.com>
2022-12-31 21:30:51 -0500

    adding info

--- src/Comp/Map/InfoBox.jsx
+++ src/Comp/Map/InfoBox.jsx
@@ -6,20 +6,22 @@ const InfoBox = () => {
             <h2>Crime Data Information </h2>
             <ul>
                 <li>ID: <strong>{info.offense_id}</strong></li>
-                <li>ID: <strong>{info.rpt_date}</strong></li>
-                <li>ID: <strong>{info.occur_date}</strong></li>
-                <li>ID: <strong>{info.occur_day}</strong></li>
-                <li>ID: <strong>{info.occur_day_num}</strong></li>
-                <li>ID: <strong>{info.occur_time}</strong></li>
-                <li>ID: <strong>{info.poss_date}</strong></li>
-                <li>ID: <strong>{info.poss_time}</strong></li>
-                <li>ID: <strong>{info.beat}</strong></li>
-                <li>ID: <strong>{info.zone}</strong></li>
-                <li>ID: <strong>{info.location}</strong></li>
+                <li>REPORTED DATE: <strong>{info.rpt_date}</strong></li>
+                <li>OCCURANCE DATE: <strong>{info.occur_date}</strong></li>
+                <li>OCCURED ON: <strong>{info.occur_day}</strong></li>
+                <li>DAY NUM: <strong>{info.occur_day_num}</strong></li>
+                <li>OCCURANCE TIME: <strong>{info.occur_time}</strong></li>
+                <li>POSS DATE: <strong>{info.poss_date}</strong></li>
+                <li>POSS TIME: <strong>{info.poss_time}</strong></li>
+                <li>BEAT: <strong>{info.beat}</strong></li>
+                <li>ZONE: <strong>{info.zone}</strong></li>
+                <li>LOCATION: <strong>{info.location}</strong></li>
                 <li>ID: <strong>{info.ibr_code}</strong></li>
                 <li>ID: <strong>{info.name}</strong></li>
-                <li>ID: <strong>{info.zone}</strong></li>
-                <li>ID: <strong>{info.zone}</strong></li>
+                <li>ID: <strong>{info.neighborhood}</strong></li>
+                <li>ID: <strong>{info.npu}</strong></li>
+                <li>ID: <strong>{info.lat}</strong></li>
+                <li>ID: <strong>{info.lng}</strong></li>
             </ul>
         </div>
 
