d0877cd44e9e25878f5030959e2888502375ee37
bd6981 <brittanyjdavis22@gmail.com>
2023-01-01 20:33:17 -0500

    working to cluster data rather than all the data rendering on top of each other

--- src/Comp/App/App.jsx
+++ src/Comp/App/App.jsx
@@ -40,8 +40,7 @@ function App() {
     <div className="App">
       <Header />
       {/* {!loading ? <GMap eventData={eventData} /> : <h1>Loading</h1>} */}
-      <GMap>
-      </GMap>
+      <GMap/>
       <Model/>
       <Nav/>
       <Footer/>
--- src/Comp/Map/GMap.jsx
+++ src/Comp/Map/GMap.jsx
@@ -1,31 +1,36 @@
 import GoogleMapReact from 'google-map-react';
-import React,{useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import './Map.css';
 import styled from "styled-components";
 import { Icon } from '@iconify/react';
 import './Map.css';
 import crimeData from '../../crimeData.json';
-import InfoBox from './InfoBox'
+import React from "react";
+import SemanticDatepicker  from 'react-semantic-ui-datepickers'
 
- const MapContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 
-function Marker ({ children }) {
-  return <Icon>{children}</Icon>;
-}
 
-
-  
 export default function GMap() {
+  const defaultProps = {
+    center: {
+      lat: 33.716073, 
+      lng: -84.353217
+    },
+    zoom: 11
+  };
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  const [q, setQ] = useState("")
-  const [searchParam] = useState(["occur_date"])
   const [data, setData] = useState([]);
+  const [date, setDate] = useState("");
+  const [searchParam] = useState(["occur_date"]);
+  const [filterParam, setFilterParam] = useState(["All"]);
+   const [currentDate, setNewDate] = useState(null);
+
+  const onChange = (e, data) => setNewDate(data.value);
+
+
   useEffect(() => {
     fetch("crimeData.json"
       , {
@@ -39,48 +44,80 @@ export default function GMap() {
         .then((res) => res.json())
         .then(
           (result) => {
-            setIsLoaded(true);
+            setIsLoaded(false);
             setData(result);
           },
           (error) => {
-            setIsLoaded(true);
+            setIsLoaded(false);
             setError(error);
           }
-        );
-  }, []);
+    )
 
-    
-    
-  const [icons, setIcons] = useState(crimeData);
-  const [infoBox, setInfoBox] = useState(null)
-   
-  const show = (name) => {
-    setIcons(crimeData.filter((item) => item.name === name))
-  };
+  }, []);  
   
-    
 
-  return (
-    <>
+  function search(data) {
+        return data.filter((crime) => {
+            if (crime.occur_date == filterParam) {
+                return searchParam.some((newItem) => {
+                    return (
+                        crime[newItem]
+                            .toString()
+                            .toLowerCase()
+                            .indexOf(date.toLowerCase()) > -1
+                    );
+                });
+            // } else if (filterParam == "All") {
+            //     return searchParam.some((newItem) => {
+            //         return (
+            //             crime[newItem]
+            //                 .toString()
+            //                 .toLowerCase()
+            //                 .indexOf(date.toLowerCase()) > -1
+            //         );
+            //     });
+            }
+        });
+    }
+
   
-      <MapContainer>
-        <GoogleMapReact bootstrapURLKeys={{ key: process.env.MY_API_KEY }}
-          zoom={10} center={[33.716073, -84.353217]}>
-          
-          {icons.map((crime => (
-            <Icon icon="uil:map-marker" className='locateIcon'
-              key={crime.id}
-              lat={crime.lat}
-              lng={crime.lng}
-              onClick = {setInfoBox}>
+    return (
+      <div className="wrapper">
+        <div className="search-wrapper">
+          <label htmlFor="search-form">
+            <input>
+            <SemanticDatepicker 
+              type="search"
+              name="search-form"
+              id="search-form"
+              className="search-input"
+              placeholder="Search for..."
+              value={date}
+              onChange={(e) => setData(e.target.value)}
+              // onChange = (event, data) => setNewDate(data.value);
+            /></input>
+          </label>
+        </div>
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyASOBWDB37dVk_DJ-EH72MhhVwD4eJLwiM" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}>
+           
+              {search(data).map((crime) => (
+              
+              <Icon icon="uil:map-marker" className='locateIcon'
+                key={crime.id}
+                lat={crime.lat}
+                lng={crime.lng}>
+              </Icon>
+            ))}
              
-            
-              {/* {crime.name} */}
-            </Icon>
-          )))} 
-        </GoogleMapReact>
-        {infoBox && <InfoBox info={infoBox} /> }
-      </MapContainer>
-    </>
-  );
-}
+          </GoogleMapReact>
+        </div>
+                
+
+      </div>
+    )
+  }
+
