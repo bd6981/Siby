d8eb68fcd63674f95d94766f37ba8c71aba7a14b
bd6981 <brittanyjdavis22@gmail.com>
2023-01-01 20:42:11 -0500

    adding context hook

---
+++ src/Comp/Map/Hooks/Hooks.js
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from 'react'
+
+const mainContext = React.createContext();
+
+export function useMainContext() {
+    return useContext(mainContext);
+}
+export function ContextProvider({ children }) {
+    const [eventData, setEventData] = useState([])
+    const [selectedEvent, setSelectedEvent] = useState(null)
+    const [reRenderMarkers, setReRenderMarkers] = useState(null)
+
+    const value = {
+        eventData,
+        setEventData,
+        selectedEvent,
+        setSelectedEvent,
+        reRenderMarkers,
+        setReRenderMarkers,
+    }
+    return (
+        <mainContext.Provider value={value}>
+            {children}
+        </mainContext.Provider>
+    )
+}
