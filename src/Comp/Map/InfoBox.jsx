4b85505a4086bbecb6ea76979c02d65295b36f4f
bd6981 <brittanyjdavis22@gmail.com>
2023-01-02 21:29:39 -0500

    Changing data key values to be easier to obtain

--- src/Comp/Map/InfoBox.jsx
+++ src/Comp/Map/InfoBox.jsx
@@ -18,22 +18,23 @@ const InfoBox = ({info}) => {
         
         <div>
             <ul>
-            <li>REPORTED DATE: <strong>{info.rpt_date}</strong></li>
-            <li>OCCURANCE DATE: <strong>{info.occur_date}</strong></li>
-            <li>OCCURED ON: <strong>{info.occur_day}</strong></li>
-            <li>DAY NUM: <strong>{info.occur_day_num}</strong></li>
-            <li>OCCURANCE TIME: <strong>{info.occur_time}</strong></li>
-            <li>POSS DATE: <strong>{info.poss_date}</strong></li>
-            <li>POSS TIME: <strong>{info.poss_time}</strong></li>
+            <li>CRIME: <strong>{info.title}</strong></li>
+            <li>ID: <strong>{info.id}</strong></li>
+            
+            <li>DATE: <strong>{info.date}</strong></li>
+            <li>OCCURED ON A: <strong>{info.day}</strong></li>
+            
+            <li>TIME: <strong>{info.occur_time}</strong></li>
+            <li>ON SCENE DATE: <strong>{info.osdate}</strong></li>
+            <li>ON SCENE TIME: <strong>{info.ostime}</strong></li>
             <li>BEAT: <strong>{info.beat}</strong></li>
             <li>ZONE: <strong>{info.zone}</strong></li>
             <li>LOCATION: <strong>{info.location}</strong></li>
-            <li>IBR CODE: <strong>{info.ibr_code}</strong></li>
-            <li>CRIME: <strong>{info.title}</strong></li>
+            <li>IBR CODE: <strong>{info.ibr}</strong></li>
+            
             <li>NEIGHBORHOOD: <strong>{info.neighborhood}</strong></li>
-            <li>NPU: <strong>{info.npu}</strong></li>
-            <li>LATITUDE: <strong>{info.latitude}</strong></li>
-            <li>LONGITUDE: <strong>{info.longitude}</strong></li>
+            
+
             </ul>
         </div>
         <footer>
