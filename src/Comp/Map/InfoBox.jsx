ca6a59604de5b971fa06e23b97167246ffb84c50
bd6981 <brittanyjdavis22@gmail.com>
2023-01-03 02:11:47 -0500

    organized file structure

--- src/Comp/Map/InfoBox.jsx
+++ src/Comp/Map/InfoBox.jsx
@@ -3,25 +3,17 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './Map.css'
 
-
-
-const InfoBox = ({info}) => {
-   
+const InfoBox = ({info}) => { 
  const [infoBox, setInfoBox] = useState(null)
-
   return (
     <>
-     <div className="infoBox">
-          <header>Crime Info</header>
-        
-        <div>
-            
+      <div className="infoBox">
+        <header>Crime Info</header>
+          <div>
             <p>CRIME: <strong>{info.title}</strong></p>
             <p>ID: <strong>{info.id}</strong></p>
-            
             <p>DATE: <strong>{info.date}</strong></p>
             <p>OCCURED ON A: <strong>{info.day}</strong></p>
-            
             <p>TIME: <strong>{info.time}</strong></p>
             <p>ON SCENE DATE: <strong>{info.osdate}</strong></p>
             <p>ON SCENE TIME: <strong>{info.ostime}</strong></p>
@@ -29,17 +21,11 @@ const InfoBox = ({info}) => {
             <p>ZONE: <strong>{info.zone}</strong></p>
             <p>LOCATION: <strong>{info.location}</strong></p>
             <p>IBR CODE: <strong>{info.ibr}</strong></p>
-            
             <p>NEIGHBORHOOD: <strong>{info.neighborhood}</strong></p>
-            
-
-            
-        </div>
-       
-
+          </div>
       </div>
     </>        
 
-    )
+  )
 }
 export default InfoBox
--- src/Comp/Map/MapMarker.js
+++ src/Comp/Map/MapMarker.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import "../../aa.png"
-import "../../bur.png"
-import "../../car.png"
-import "../../h.png"
-import "../../rob.png"
-import "../../theft.png"
+import React, {useState} from 'react'
+import "../PNG/aa.png"
+import "../PNG/bur.png"
+import "../PNG/car.png"
+import "../PNG/h.png"
+import "../PNG/rob.png"
+import "../PNG/theft.png"
 
 
-const MapMarker = (lat, lng, onClick, name) => {
+const MapMarker = (lat, lng, onClick, title) => {
+const [icon, setIcon] = useState(null)
     let renderIcon = null;
     if (title === "BURGLARY")
-        renderIcon = robberIcon;
+        renderIcon = "../PNG/bur.png";
     else if (title === "AUTO THEFT")
-        renderIcon = keyCard;
+        renderIcon = "../PNG/car.png";
     else if (title === "HOMICIDE")
-        renderIcon = crimeSceneTape;
-        else if (title === "HOMICIDE")
-        renderIcon = crimeSceneTape;
-        else if (title === "HOMICIDE")
-        renderIcon = crimeSceneTape;
-        else if (title === "HOMICIDE")
-        renderIcon = crimeSceneTape;
+        renderIcon = "../PNG/h.png";
+    else if (title === "LARCENY-NON VEHICLE")
+        renderIcon = "../PNG/theft.png";
+    else if (title === "LARCENY-FROM VEHICLE")
+        renderIcon = "../PNG/car.png";
+    else if (title === "ROBBERY")
+        renderIcon = "../PNG/rob.png";
+    else if (title === "AGG ASSAULT")
+        renderIcon = "../PNG/aa.png";
     
   return (
-      <div onClick={onClick}>
-          <Icon icon={renderIcon} className="map-marker"/>
-         </div>
+    <div onClick={onClick}>
+        <MapMarker icon={renderIcon} className="map-marker"/>
+    </div>
   )
 }
 
