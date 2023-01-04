c3f01a5509f9c8b5b7575ede9d2806a2f17358da
bd6981 <brittanyjdavis22@gmail.com>
2023-01-03 23:04:57 -0500

    added to hooks

--- src/Comp/Map/Hooks/Hooks.js
+++ src/Comp/Map/Hooks/Hooks.js
@@ -1,3 +1,4 @@
+import { createStore } from 'easy-peasy';
 import React, { useContext, useState } from 'react'
 
 const mainContext = React.createContext();
@@ -24,3 +25,15 @@ export function ContextProvider({ children }) {
         </mainContext.Provider>
     )
 }
+import { createStore, action } from "easy-peasy"
+const EasyPeasy = createStore({
+    authorized: false,
+    setAuthorized: action((state, payload) => {
+        state.authorized = true;
+
+    }),
+    setUnauthorized: action((state, payload) => {
+        state.authorized = false;
+    })
+});
+export default EasyPeasy
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Comp/App/App';
-import {ContextProvider} from './Comp/Map/Hooks/Hooks'
+import { ContextProvider, EasyPeasy } from './Comp/Map/Hooks/Hooks'
+import { StoreProvider } from "easy-peasy"
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <StoreProvider EasyPeasy={EasyPeasy}>
     <ContextProvider>
       <App />
     </ContextProvider>
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
