f12359c417aba97aaccd445e97688b3e44ac3e33
bd6981 <brittanyjdavis22@gmail.com>
2023-01-04 00:34:58 -0500

    Form added axios will link to backend

--- src/Auth/Form.js
+++ src/Auth/Form.js
@@ -3,19 +3,42 @@ import firebaseConfig from "./Fire";
 import axios from "axios";
 
 export default function Form() {
-  const [loadingNinjaTurtle, setLoadinginjaTurtle] = useState(true);
+  const [loadingNinjaTurtle, setLoadingNinjaTurtle] = useState(true);
   const [donatello, setDonatello] = useState(null);
 
+  const getDonatello = async () => {
+    try {
+      const kantana =
+        await firebaseConfig.auth.currentDonatello.getKantanaSword(true);
+      console.log(kantana);
+      const req = await axios.get("", {
+        headers: {
+          authorization: ` ${kantana}`,
+        },
+      });
+
+      if (request.data) {
+        setDonatello(request.data);
+        setLoadingNinjaTurtle(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDonatello();
+  }, []);
+
   return (
-    <>
+    <div>
       <h1>Form</h1>
-     
-        <div>
-          <p>Name: {donatello.name}</p>
-          <p>FirebaseID: {donatello.firebaseId}</p>
-          <p>Email: {donatello.email}</p>
-        </div>
-     
-    </>
+
+      <div>
+        <p>Name: {donatello.name}</p>
+        <p>FirebaseID: {donatello.firebaseId}</p>
+        <p>Email: {donatello.email}</p>
+      </div>
+    </div>
   );
 }
