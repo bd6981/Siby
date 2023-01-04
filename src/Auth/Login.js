89cfd82ae40f2f5885b999f16270cde9b1a5c2f9
bd6981 <brittanyjdavis22@gmail.com>
2023-01-03 23:32:07 -0500

    adding to login handle errors

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -11,51 +11,52 @@ export default function Login() {
         email: "",
         password: "",
     });
-   
+    const [error, setError] = useState("");
+      const setAuthorized = useStoreActions((actions) => actions.setAuthorized);
         const handleChange = (event) => {
           setFields({ [event.target.name]: event.target.value });
         };
 
-        const handleSubmit = async (event) => {
-          event.preventDefault();
-            try {
-                const user = await signInWithEmailAndPassword(
-                    firebaseService.auth,
-                    email,
-                    password
-                );
-                if (user) {
-                    setAuthorized();
-                    navigate("/");
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const user = await signInWithEmailAndPassword(
+                firebaseService.auth,
+                email,
+                password
+            );
+            if (user) {
+                setAuthorized();
+                navigate("/");
               
-                }
+            }
             
         };
 
-    return (
-        <main>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email Address</label>
-                </div>
-                <div>
-                    <input
-                        type="email"
-                        name="email" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        name="password" />
-                </div>
-                <div>
-                    <button type="submit">Login</button>
-                </div>
-            </form>
-        </main>
-    );
-}
+        return (
+            <main>
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                    </div>
+                    <div>
+                        <input
+                            type="email"
+                            name="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            name="password" />
+                    </div>
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </main>
+        )
+    }
